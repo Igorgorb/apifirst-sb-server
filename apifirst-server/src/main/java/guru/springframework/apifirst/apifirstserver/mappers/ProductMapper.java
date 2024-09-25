1c1d6383cd6108f29a710b0f266718546159044a
Ihor Horbonos <igorgorb@gmail.com>
2024-09-25 17:51:17 +0300

    change product mapper

--- apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/mappers/ProductMapper.java
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/mappers/ProductMapper.java
@@ -4,14 +4,30 @@ package guru.springframework.apifirst.apifirstserver.mappers;
 import guru.springframework.apifirst.apifirstserver.domain.Product;
 import guru.springframework.apifirst.model.ProductCreateDto;
 import guru.springframework.apifirst.model.ProductDto;
+import guru.springframework.apifirst.model.ProductUpdateDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper
 @DecoratedWith(ProductMapperDecorator.class)
 public interface ProductMapper {
 
+    @Mapping(target = "categories", ignore = true)
+    ProductUpdateDto productToUpdateDto(Product product);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    @Mapping(target = "dateUpdated", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    Product dtoToProduct(ProductUpdateDto productUpdateDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    @Mapping(target = "dateUpdated", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    void updateProduct(ProductUpdateDto productUpdateDto, @MappingTarget Product product);
 
     @Mapping(target = "dateCreated", ignore = true)
     @Mapping(target = "dateUpdated", ignore = true)
---
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/repositories/ImageRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.apifirst.apifirstserver.repositories;
+
+import guru.springframework.apifirst.apifirstserver.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ImageRepository extends JpaRepository<Image, UUID> {
+}
